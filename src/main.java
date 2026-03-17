39636b96f1086fd38999093f272ee534a7d92ef4
baro1chuo <lychibao200t@gmail.com>
2026-03-17 16:31:18 +0700

    test

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("TÉT");
         System.out.println("vcl");
+        System.out.println("vcl");
     }
 }
